153c575302f8bc5e8b6acb2710ce028defaf4502
sumitpandit <sumitpandit178@gmail.com>
2020-09-30 20:47:57 +0530

    [Sumit] Add: Snake and Ladder game with single Player at Start Position

---
+++ SankeAndLadderGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SankeAndLadderGame
+{
+    class Program
+    {
+        public const int NO_OF_PLAYERS = 1;
+        public const int INITIAL_POSITION = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Game!!!");
+            Console.WriteLine("Player is at the starting position");
+        }
+    }
+}
