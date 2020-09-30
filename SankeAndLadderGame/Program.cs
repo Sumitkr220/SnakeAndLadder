8cb79f8027ca6ba3cd9b7b9a5460df26922dc14c
sumitpandit <sumitpandit178@gmail.com>
2020-09-30 21:20:11 +0530

    [Sumit] Refactor: Repeat till the player reaches the winning position 100

--- SankeAndLadderGame/Program.cs
+++ SankeAndLadderGame/Program.cs
@@ -9,32 +9,37 @@ namespace SankeAndLadderGame
 		public const int NO_PLAY = 0;
 		public const int LADDER = 1;
 		public const int SNAKE = 2;
+		public const int WINNING_POSITION = 100;
 		static void Main(String[] args)
 		{
 			int currPosition = INITIAL_POSITION;
 			Console.WriteLine("Welcome to Snake Ladder Game!!!");
 			Console.WriteLine("Player is at the starting position");
-			Random random = new Random();
-			int diceValue = random.Next(1, 7);
-			Console.WriteLine("Player rolled : " + diceValue);
-			int actionTaken = random.Next(0, 3);
-			if (actionTaken == NO_PLAY)
+			while (currPosition < WINNING_POSITION)
 			{
-				Console.WriteLine("No action taken");
-			}
-			else if (actionTaken == LADDER)
-			{
-				currPosition += diceValue;
-			}
-			else
-			{
-				currPosition -= diceValue;
-				if (currPosition < INITIAL_POSITION)
+				Random random = new Random();
+				int diceValue = random.Next(1, 7);
+				Console.WriteLine("Player rolled : " + diceValue);
+				int actionTaken = random.Next(0, 3);
+				if (actionTaken == NO_PLAY)
 				{
-					currPosition = INITIAL_POSITION;
+					Console.WriteLine("No action taken");
 				}
+				else if (actionTaken == LADDER)
+				{
+					currPosition += diceValue;
+				}
+				else
+				{
+					currPosition -= diceValue;
+					if (currPosition < INITIAL_POSITION)
+					{
+						currPosition = INITIAL_POSITION;
+					}
+				}
+				Console.WriteLine("Player is at position : " + currPosition);
 			}
-			Console.WriteLine("Player is at position : " + currPosition);
+			Console.WriteLine("Player reaches the winning position");
 		}
 	}
 }
